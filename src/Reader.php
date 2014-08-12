8e399ac59f40be729fc8fb018b6013a90a182316
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2014-08-13 01:25:48 +0800

    update reader

--- src/Reader.php
+++ src/Reader.php
@@ -1,5 +1,6 @@
 <?php namespace Oven;
 
+use Illuminate\Support\Arr;
 use Illuminate\Filesystem\Filesystem;
 
 class Reader {
@@ -7,6 +8,8 @@ class Reader {
     const RECIPEFILE = 'recipe.json';
 
     protected $file;
+
+    protected $recipe;
 
     public function __construct(Filesystem $file)
     {
@@ -17,14 +20,43 @@ class Reader {
     {
         $file = is_null($file) ? __DIR__.'/'.self::RECIPEFILE : $file;
 
+        return $this->validateRecipe($file);
+    }
+
+    public function getFilesystem()
+    {
+        return $this->file;
+    }
+
+    public function getRecipeName()
+    {   
+        return Arr::get($this->recipe, 'name', null);
+    }
+
+    public function getIngredient($key)
+    {
+        return Arr::get($this->recipe, "ingredients.{$key}", null);
+    }
+
+    protected function validateRecipe($file)
+    {
         if ( ! $this->file->exists($file)) {
             return 'Error! Cannot load recipe file. Make sure the path is correct';
         }
-    }
 
-    protected function validateRecipe()
-    {
+        $recipe = json_decode($this->file->get($file), true);
 
+        if ( ! is_array($recipe)) {
+            return 'Error! Invalid recipe file';
+        }
+
+        if ( ! array_key_exists('ingredients', $recipe)) {
+            return 'Error! Your recipe file is missing the ingredients';
+        }
+
+        $this->recipe = $recipe;
+
+        return true;
     }
 
 }
